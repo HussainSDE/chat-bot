b92f57905b7fcaa9f26a0da71c08bfb2d0016905
MohammadHussain1218 <mdhussain1218@gmail.com>
2026-09-14 13:47:44 +0530

    Application structure commit -1

---
+++ src/test/java/com/chat/chat_bot_app/ChatBotAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.chat_bot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
